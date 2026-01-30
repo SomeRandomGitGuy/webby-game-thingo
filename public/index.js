8876c69173da9945502020d46afcb1c7031f1aa9
Grant Fitzgibbon <grant.fitzgibbon@gmail.com>
2026-01-30 20:23:13 +1100

    oshan how to do fetch()?

---
+++ public/index.js
@@ -0,0 +1,19 @@
+
+
+
+async function getData() {
+  const url = "db/stocks.json";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+await getData();
